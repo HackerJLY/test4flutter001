16e1d8260d8735548dfef1f91b94361cbf59fc70
HackerJLY <HackerJLY@qq.com>
2026-03-16 11:25:20 +0800

    删除不需要上传git的文件

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=D:\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\JiFei\Projects_Me\Flutter_Projects\test4flutter001"
+export "FLUTTER_APPLICATION_PATH=D:\JiFei_ZG\SourceCode\test4flutter001"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
